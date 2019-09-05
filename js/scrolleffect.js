76104a03b8f98d257f048353f71b945a7eff43fb
ken2slink <ken2slink@hotmail.fr>
2019-09-05 15:27:37 +0200

    basic scroll effect on experience

--- js/scrolleffect.js
+++ js/scrolleffect.js
@@ -1,38 +1,34 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+//tween
+
+var tl = new TimelineMax();
+tl.from(".timeline-badge", 1, {x:-100, opacity:0});
+
+var animtext = new TimelineMax();
+
+var tween1 = TweenMax.from(".timeline-panel-container", 1, {x:800, opacity:0});
+var tween2 = TweenMax.from(".timeline-panel-container-inverted", 1, {x:-800, opacity:0});
+animtext.add(tween1);
+animtext.add(tween2);
 
 
-var tl = new TimelineMax({onUpdate:updatePercentage});
-var tl2 = new TimelineMax();
-const controller = new ScrollMagic.Controller();
+// create a scene
+const scene = new ScrollMagic.Scene({
+		duration: "80%",	// the scene should last for a scroll distance of 100px
+		triggerElement: ".first-badge",
+		triggerHook: "onEnter"
+	})
+	// .setPin(".sticky-effect"); // pins the element for the the scene's duration
+	.setTween(tl)
+	.addTo(controller); // assign the scene to the controller
 
-// tl.from('#span', 1, { width: 0}, "=-.5");
-tl.from('#timeline-panel-container', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
-tl.from('#timeline-badge', 1, {x:200, opacity: 0,ease: Power4.easeInOut}, "=-.7");
+const scene2 = new ScrollMagic.Scene({
+	duration: "80%",
+	triggerElement: ".first-badge",
+	triggerHook: "onEnter"
+    })
 
-tl2.from("#box", 1, {opacity:0, scale: 0});
-tl2.to("#box", 0.5, {
-	left:"20%",
-	scale:1.3,
-	borderColor: 'white',
-	borderWidth:12,
-	boxShadow: '1px 1px 0px 0px rgba(0,0,0,.09)'
-});
-
-// const scene = new ScrollMagic.Scene({
-// 	triggerElement: ".sticky",
-// 	triggerHook: "onLeave",
-// 	duration: "100%"
-// })
-
-//   .setPin(".sticky")
-//   .setTween(tl)
-//   .addTo(controller);
-
-
-//   .setTween(tl2)
-//   .addTo(controller);
-
-//  function updatePercentage() {
-//  	tl.progress();
-//  	console.log(tl.progress());
-
-//  }
+	.setTween(animtext)
+	.addTo(controller);
